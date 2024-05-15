f91dc7c02acff53aa629462d98236d7fa0e516b1
jpsalis <jpsalis@outlook.com>
2024-05-15 19:57:50 -0400

    working lidar display

--- src/lidar.py
+++ src/lidar.py
@@ -1,38 +1,68 @@
-from math import floor
-from rplidar import RPLidar
+"""
+Consume LIDAR measurement file and create an image for display.
+
+Adafruit invests time and resources providing this open source code.
+Please support Adafruit and open source hardware by purchasing
+products from Adafruit!
+
+Written by Dave Astels for Adafruit Industries
+Copyright (c) 2019 Adafruit Industries
+Licensed under the MIT license.
+
+All text above must be included in any redistribution.
+"""
+
+import os
+from math import cos, sin, pi, floor
+import pygame
+from adafruit_rplidar import RPLidar
+import time
+
+# Set up pygame and the display
+os.putenv('SDL_FBDEV', '/dev/fb1')
+pygame.init()
+lcd = pygame.display.set_mode((320,240))
+pygame.mouse.set_visible(False)
+lcd.fill((0,0,0))
+pygame.display.update()
 
 # Setup the RPLidar
-PORT_NAME = "/dev/ttyUSB0"
-lidar = RPLidar(PORT_NAME, timeout=3, baudrate = 115200)
-
+PORT_NAME = '/dev/ttyUSB0'
+lidar = RPLidar(None, PORT_NAME, baudrate=115200, timeout=3)
 
 # used to scale data to fit on the screen
 max_distance = 0
-MIN = 0
-MAX = 20
 
+#pylint: disable=redefined-outer-name,global-statement
 def process_data(data):
-    # Look for values in range, average sum of distance
-    sum = 0
-    count = 0
-    for item in data:
-        if item['angle'] >= MIN and item['angle'] <= MAX:
-            sum += item['distance']
-            count += 1
-    if count > 0:
-        print(sum / count)
-            
 
-scan_data = []
+    global max_distance
+    lcd.fill((0,0,0))
+    for angle in range(360):
+        distance = data[angle]
+        if distance > 0:                  # ignore initially ungathered data points
+            max_distance = max([min([5000, distance]), max_distance])
+            radians = angle * pi / 180.0
+            x = distance * cos(radians)
+            y = distance * sin(radians)
+            point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance * 119))
+            lcd.set_at(point, pygame.Color(255, 255, 255))
+    pygame.display.update()
+
+
+
+scan_data = [0]*360
+
 try:
-    #    print(lidar.get_info())
+    #print(lidar.info)
+    #lidar.start()
     for scan in lidar.iter_scans():
-        scan_data.clear()
-        for _, angle, distance in scan:
-            scan_data.append({'angle':min(359, floor(angle)), 'distance':distance})
+        for (_, angle, distance) in scan:
+            scan_data[min([359, floor(angle)])] = distance
         process_data(scan_data)
 
 except KeyboardInterrupt:
-    print("Stopping.")
+    print('Stoping.')
+
 lidar.stop()
 lidar.disconnect()
